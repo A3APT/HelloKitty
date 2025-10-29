7726bd07517fe105c1c14fdc9df1cb844f11b9d2
A3APT <nawshadfahim1@gmail.com>
2025-10-29 17:49:28 +0100

    apatoto ekta try korlam

--- HelloKitty/src/Main.java
+++ HelloKitty/src/Main.java
@@ -3,12 +3,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("What's your name?");
+
+        System.out.println("Enter your name:");
         String name = scanner.nextLine();
-        
-        System.out.println("Hello, " + name + "! Did you know that if you reverse your name it sounds like '" + new StringBuilder(name).reverse().toString() + "'? Fun, right?");
-        
+
+        if (name.equals("xxx")) {
+            System.out.println("🙄😠 G4rbl3d m3ss4g3! You broke it! 😡🤬");
+        } else {
+            String reversedName = new StringBuilder(name).reverse().toString();
+            System.out.println("Your name reversed is: " + reversedName);
+
+            NewKitty newKitty = new NewKitty();
+            newKitty.createOrRenameFolder(name);
+        }
+
         scanner.close();
     }
 }
--- HelloKitty/src/NewKitty.java
+++ HelloKitty/src/NewKitty.java
@@ -2,30 +2,53 @@ import java.io.File;
 import java.io.IOException;
 
 public class NewKitty {
-    public static void main(String[] args) {
-        // Get the desktop path
-        String desktopPath = System.getProperty("user.home") + File.separator + "Desktop";
 
-        // Create folder on desktop
-        File folder = new File(desktopPath, "HelloKittyFolder");
-        if (!folder.exists()) {
-            boolean created = folder.mkdir();
-            if (created) {
-                System.out.println("Folder 'HelloKittyFolder' created on Desktop.");
+    private File folder;
+    private final String desktopPath = System.getProperty("user.home") + File.separator + "Desktop";
+
+    public void createOrRenameFolder(String folderName) {
+        // Define folder with requested name
+        File newFolder = new File(desktopPath, folderName);
+
+        // Check if a folder with a different name exists, rename it
+        if (folder != null && folder.exists() && !folder.getName().equals(folderName)) {
+            boolean renamed = folder.renameTo(newFolder);
+            if (renamed) {
+                System.out.println("Folder renamed to: " + folderName);
             } else {
-                System.out.println("Failed to create folder on Desktop.");
+                System.out.println("Failed to rename the folder.");
             }
-        } else {
-            System.out.println("Folder already exists on Desktop.");
         }
 
-        // Create file inside the folder on desktop
+        // If folder doesn't exist, create it
+        if (!newFolder.exists()) {
+            boolean created = newFolder.mkdir();
+            if (created) {
+                System.out.println("Folder '" + folderName + "' created on Desktop.");
+            } else {
+                System.out.println("Failed to create folder '" + folderName + "'.");
+            }
+        } else {
+            System.out.println("Folder '" + folderName + "' already exists.");
+        }
+
+        folder = newFolder;
+
+        createFileInFolder();
+    }
+
+    private void createFileInFolder() {
+        if (folder == null) {
+            System.out.println("Folder not set. Cannot create file.");
+            return;
+        }
+
         File file = new File(folder, "hello kitty.txt");
         try {
             if (file.createNewFile()) {
-                System.out.println("File 'hello kitty.txt' created inside 'HelloKittyFolder' on Desktop.");
+                System.out.println("File 'hello kitty.txt' created inside folder '" + folder.getName() + "'.");
             } else {
-                System.out.println("File already exists.");
+                System.out.println("File 'hello kitty.txt' already exists inside folder '" + folder.getName() + "'.");
             }
         } catch (IOException e) {
             System.out.println("An error occurred creating the file.");
